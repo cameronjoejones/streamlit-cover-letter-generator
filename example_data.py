769f4de076e5b8f84c85aa1935b68964f1a7097c
Cameron <85755746+cameronjoej@users.noreply.github.com>
2023-03-07 00:21:17 +0000

    Create example_data.py

---
+++ example_data.py
@@ -0,0 +1,79 @@
+example_cv = """Lilibeth Andrada
+Data Analyst
+Motivated, teamwork-oriented, and responsible Data Analyst with significant experience in increasing comprehension of reports and presentations by the average professional. Highly educated, possessing a Bachelor's, a Master's, and professional certification in business analytics and statistics. Bilingual in English and Tagalog, with an intermediate understanding of Spanish.
+lilibeth@andrada.com
+TECHNICAL (IT) SKILLS
+Data
+Management
+Computer
+Science
+Research
+Database Design & Management, Data Quality Assessment, Data Analysis, Pattern & Trend Identification, Visualization of Data Insights
+System Administration, Advanced Microsoft Excel Functions, Sharepoint, MySQL, Tableau, Python, Troubleshooting
+Data Science Research Methods, Data Mining, Survey Creation, Focus Groups
+WORK EXPERIENCE
+IT Business Operations Data Analyst
+Dell Technologies
+04/2014 - Present
+- Converted data into actionable insights by predicting and modeling future outcomes.
+- Utilized MS SQL, data warehousing programs, Tableau, and other dashboard/visualization toolsets for data intelligence and analysis.
+- Successfully adhered to company data governance standards receiving the "Employee of the Year" for the last 2 years in a row.
+Market Research Analyst
+Pearson & Associates
+11/2010 - 03/2014
+- Identified, analyzed, and executed new and potential products, services, markets, and advertising opportunities.
+- Collected and analyzed data on established and prospective customers, competitors, and marketing channels and sources.
+- Prepared reports that interpret consumer behavior, market opportunities and conditions, marketing results, trends, and investment levels.
+- Utilized Google Analytics and Google Tag Manager and implemented new scripts that increased performance by 25%.
+EDUCATION
+MBA - Concentration in Business Analytics
+Universit of Texas at Austin
+08/2008 - 06/2010
+Bachelor's in Statistical Science
+Southern Methodist Universit
+08/2005 - 06/2008
+PROFESSIONAL CERTIFICATES
+Certification of Professional Achievement in Data
+Sciences (2016)
+Online course - Columbia University
+EMC Proven Professional Data Scientist Associate
+(2014)
+"""
+
+example_job_description = """Technical Company is hiring for a new data analyst role!
+
+About the job
+
+Data Analyst / Insight Analyst / SQL Data Analyst / Analytics Executive
+
+
+Office based in central London, 4 days on site/1 day working from home. Initially 12 months full time contract and possible to become permanent afterwards, contract is inside IR35.
+
+
+Snap is looking for a Data Analyst to join their London Ad and business team where you will work closely with sales, account managers and clients to provide in-depth insight analysis and drive advertising performance, business revenue and growth:
+
+
+-Build dashboards, reports and insight analysis using SQL, Big Query, Tableau, Looker and Google Data Studio to optimize advertiser performance, product development and SME business growth;
+
+-Create data tools for the central Europe region;
+
+-Build and automate processes that can be scaled out for stakeholders and cross functional teams;
+
+-Work with Account Managers to provide real time analysis for campaigns and impact client and business success by providing analysis and recommendations;
+
+-Carry out quantitative data research
+
+
+Requirements:
+
+-Must have strong SQL experience;
+
+-Experience in creating data tool and automation that can be scaled out;
+
+-Experience in R or Python is a big plus;
+
+-Any working knowledge in advertising or performance or media or sales analysis is a big plus
+
+
+
+"""
